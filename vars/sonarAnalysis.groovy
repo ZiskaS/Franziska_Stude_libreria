0ec29f3a50cec7e90f2266e2159ec44554b66742
¨ZiskaS¨ <franziska.stude@gmx.de>
2024-04-14 19:46:43 +0200

    new sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,14 +1,25 @@
 def call(boolean abortPipeline = false, int timeout = 5) {
-    // Implementación de la función sonarAnalysis
-    // Puedes reemplazar este bloque con la lógica necesaria para ejecutar el escaneo de SonarQube
-    // o mostrar un mensaje de prueba
-    echo "Ejecución de las pruebas de calidad de código"
-    
-    // Simulación de tiempo de espera (timeout)
-    sleep time: timeout, unit: 'MINUTES'
-    
-    // Verificación del parámetro abortPipeline
-    if (abortPipeline) {
-        error 'El pipeline debe ser abortado según la configuración'
+    timeout = timeout ?: 5 // Si no se proporciona un valor para timeout, se establece en 5 minutos por defecto
+    try {
+        // Simulando la ejecución de las pruebas de calidad de código
+        sh 'echo "Ejecución de las pruebas de calidad de código"'
+
+        // Esperar al resultado
+        timeout(time: timeout, unit: 'MINUTES') {
+            // Lógica para evaluar el QualityGate de SonarQube
+            // Si el QualityGate no se pasa y abortPipeline es verdadero, abortar el pipeline
+            // De lo contrario, continuar con el pipeline
+            // Código de ejemplo:
+            // if (qualityGateFails && abortPipeline) {
+            //     error "QualityGate de SonarQube no se pasa. Abortando el pipeline."
+            // }
+        }
+    } catch (Exception e) {
+        // Si hay alguna excepción, manejarla aquí
+        echo "Error: ${e.message}"
+        // Si abortPipeline es verdadero, abortar el pipeline
+        if (abortPipeline) {
+            error "Error durante la ejecución de las pruebas de calidad de código. Abortando el pipeline."
+        }
     }
 }
