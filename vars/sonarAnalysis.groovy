94fbd0dfaf02dc3c294074c129e0e57898815ef6
¨ZiskaS¨ <franziska.stude@gmx.de>
2024-04-14 18:47:55 +0200

    anadir archivi nuevo

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,12 +1,14 @@
-pipeline {
-    agent any
-    stages {
-        stage('SonarQube Analysis') {
-            steps {
-                script {
-                    sonarAnalysis(abortPipeline: true, timeout: 5)
-                }
-            }
+def call(boolean abortPipeline = false, int timeout = 5) {
+    // Ejecutar el escaneo de SonarQube (o mostrar un mensaje de prueba)
+    echo "Ejecución de las pruebas de calidad de código"
+    
+    // Esperar durante el tiempo especificado (en minutos) con un timeout
+    timeout(time: timeout, unit: 'MINUTES') {
+        // Evaluar el QualityGate de SonarQube y determinar si se debe abortar el pipeline
+        if (abortPipeline) {
+            error "QualityGate de SonarQube ha fallado. Se aborta el pipeline."
+        } else {
+            echo "QualityGate de SonarQube ha pasado. Se continúa con el pipeline."
         }
     }
 }
